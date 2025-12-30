34537a27e7096de1083a7f6b7b7a1180acd404ee
Travis Dockter <travisdock@gmail.com>
2025-12-30 07:05:56 -0700

    Add validation to resources url

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -3,7 +3,13 @@ class Resource < ApplicationRecord
 
   validate :content_or_url_present
   validates :content, length: { maximum: 5000 }, allow_blank: true
-  validates :url, length: { maximum: 2000 }, allow_blank: true
+  validates :url,
+            length: { maximum: 2000 },
+            allow_blank: true,
+            format: {
+              with: URI::DEFAULT_PARSER.make_regexp(%w[http https]),
+              message: "must be a valid HTTP or HTTPS URL"
+            }
 
   scope :last_two, -> { order(created_at: :desc).limit(2) }
 
